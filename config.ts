7069931c2a04d4aaaea34d50fa55ed8e1f4fa3c7
Matan Yemini <matan@engageli.com>
2023-11-19 09:29:15 +0000

    Update config with Engageli support base url

--- config.ts
+++ config.ts
@@ -12,20 +12,20 @@ type Config = {
   /** File name for the finished data */
   outputFileName: string;
   /** Optional cookie to be set. E.g. for Cookie Consent */
-  cookie?: {name: string; value: string}
+  cookie?: { name: string; value: string };
   /** Optional function to run for each page found */
   onVisitPage?: (options: {
     page: Page;
     pushData: (data: any) => Promise<void>;
   }) => Promise<void>;
-    /** Optional timeout for waiting for a selector to appear */
+  /** Optional timeout for waiting for a selector to appear */
   waitForSelectorTimeout?: number;
 };
 
 export const config: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  selector: `.docs-builder-container`,
-  maxPagesToCrawl: 50,
+  url: "https://support.engageli.com/hc/en-us/articles/18318598994587-Step-1-Login-and-launch-your-Engageli-classroom",
+  match: "https://support.engageli.com/hc/en-us/**",
+  selector: `.article__wrapper`,
+  maxPagesToCrawl: 1500,
   outputFileName: "output.json",
 };
